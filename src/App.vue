272951600d1770f614a067b9792732356e47131d
Kamrul <khannahid361@gmail.com>
2026-03-29 16:43:43 +0600

    Accessing Store state

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,19 @@
       <img src="./assets/pinia-logo.svg" alt="Pinia Logo">
       <h1>Pinia To-Do List</h1>
     </header>
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <taskDetails :task="task" />
+      </div>
+    </div>
   </main>
 </template>
 
-<script setup></script>
+<script setup>
+  import { useTaskStore } from "./stores/taskStore.js"
+  import taskDetails from "./components/taskDetails.vue"
+  const taskStore = useTaskStore()
+</script>
 
 <style scoped>
 
---
+++ src/components/taskDetails.vue
@@ -0,0 +1,17 @@
+<script setup>
+    defineProps(['task'])
+</script>
+
+<template>
+    <div class="task">
+        <h3>{{ task.title }}</h3>
+        <dv class="icons">
+            <i class="material-icons">delete</i>
+            <i class="material-icons">favorite</i>
+        </dv>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
