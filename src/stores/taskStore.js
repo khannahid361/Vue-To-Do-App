cc0b11511b23eccdf67109dba2d87dd4f912fb50
Kamrul <khannahid361@gmail.com>
2026-04-02 09:09:49 +0600

    Refactor task management methods to be asynchronous and handle API responses for add, delete, and toggle favorite actions

--- src/stores/taskStore.js
+++ src/stores/taskStore.js
@@ -14,17 +14,41 @@ export const useTaskStore = defineStore("taskStore", {
       this.tasks = data
       this.loading = false
     },
-    addTask(task) {
+    async addTask(task) {
       this.tasks.push(task);
-    },
-    deleteTask(id){
-      this.tasks = this.tasks.filter(t => {
-        return t.id!=id
+
+      const res = await fetch("http://localhost:3000/tasks", {
+        method: "POST",
+        body: JSON.stringify(task),
+        headers: {
+          "Content-Type": "application/json"
+        }
       })
+      if (res.error) {
+        console.log("Error adding task")
+      }
     },
-    toggleFav(id){
+    async deleteTask(id) {
+      this.tasks = this.tasks.filter(t => {
+        return t.id != id
+      })
+      const res = await fetch("http://localhost:3000/tasks/" + id, {
+        method: "DELETE"
+      })
+      if (res.error) {
+        console.log("Error deleting task")
+      }
+    },
+    async toggleFav(id) {
       const task = this.tasks.find(t => t.id === id)
       task.isFav = !task.isFav
+      const res = await fetch("http://localhost:3000/tasks/" + id, {
+        method: "PATCH",
+        body: JSON.stringify({ isFav: task.isFav }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
     }
   },
   getters: {
@@ -39,7 +63,7 @@ export const useTaskStore = defineStore("taskStore", {
         return currentValue.isFav ? prevValue + 1 : prevValue
       }, 0)
     },
-    totalCount:(state) => {
+    totalCount: (state) => {
       return state.tasks.length
     }
   },
